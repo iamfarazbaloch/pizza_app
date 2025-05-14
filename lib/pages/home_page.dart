2c4ca40f64a672f359d46a03bd248be78b5833db
Faraz Ahmad <faraz.ahmad0366@gmail.com>
2025-05-14 08:51:29 +0500

    custom app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,62 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: []));
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset('assets/icons/slice.png'),
+                      Gap(4),
+                      Text(
+                        'Pizza',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFF4900C),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on_outlined,
+                        color: Color(0xFFF4900C),
+                      ),
+                      Text(
+                        'Cairo,',
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                      Gap(4),
+                      Image.asset('assets/icons/fg.png'),
+                      Icon(Icons.keyboard_arrow_down),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFDFDFDF),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Icon(Icons.favorite_border_outlined, size: 24),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
